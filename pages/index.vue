047c7aa59441b6be339e99d72dfc5f82ad5c1a40
y-mitsuyoshi <glorymanchesteru@gmail.com>
2025-03-09 11:26:58 +0900

    index page作成

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome to Deepfake Liveness Detection</h1>
+    <NuxtLink to="/face-liveness">Go to Face Liveness Detection</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+// No additional script needed
+</script>
+
+<style scoped>
+div {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+</style>
